00c34b0866c63484670057d1c1234c4de71e4e98
bbster <bbster12@naver.com>
2020-02-05 19:07:14 +0900

    증감 연산자

--- Project_2020_02_05/src/InputTest.java
+++ Project_2020_02_05/src/InputTest.java
@@ -3,12 +3,10 @@ import java.util.*;
 public class InputTest {
 
 	public static void main(String[] args) {
-		System.out.printf("원하는 숫자를 입력하세요. \n");
+		System.out.printf("생년원일 입력. \n");
 		
 		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		System.out.printf("입력하신 숫자는 : %d", num);
-
+		String num = scanner.nextLine();
+		System.out.printf("생년월일은 : %s", num);
 	}
-
 }

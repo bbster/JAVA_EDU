9a2a083af65cb31b79cda925f14583590d82b92c
bbster <bbster12@naver.com>
2020-02-06 15:35:32 +0900

    for문

--- Project_2020_02_06/src/Score.java
+++ Project_2020_02_06/src/Score.java
@@ -7,16 +7,37 @@ public class Score {
 		
 		Scanner scanner = new Scanner(System.in);
 		int score = scanner.nextInt();
+		int sum=0;
+		char grade;
 		
-		if(score>0) {
-			System.out.printf("양수");
-		}
-		else if(score<0) {
-			System.out.printf("음수");
-		}
+//		if(score>0) {
+//			System.out.printf("양수");
+//		}
+//		else if(score<0) {
+//			System.out.printf("음수");
+//		}
+//		
+//		else {
+//			System.out.printf("0입니다.");
+//		}
 		
-		else {
-			System.out.printf("0입니다.");
+//		if(score>=90) {
+//			System.out.printf("A등급");
+//			grade = 'A';
+//		}
+//		else if(score>=80 && score<=89) {
+//			System.out.printf("B등급");
+//			grade = 'B';
+//		}
+//		else {
+//			System.out.printf("C등급");
+//			grade = 'C';
+//		}
+//		System.out.println(grade);	
+		
+		for(int i=0; i<=10; i++) {
+			sum += i;
+			System.out.println(sum);
 		}
 	}
 }

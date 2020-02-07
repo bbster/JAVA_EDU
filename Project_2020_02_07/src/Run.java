7a4ad069047d0a13d084e70e8094c624041c7e0b
bbster <bbster12@naver.com>
2020-02-07 16:58:41 +0900

    기본형 매개변수

--- Project_2020_02_07/src/Run.java
+++ Project_2020_02_07/src/Run.java
@@ -21,20 +21,35 @@ public class Run {
 		
 		//-----------------------------------------------------------------------------//
 		
-		CardTest c1 = new CardTest();
-		c1.kind = "Heart";
-		c1.number = 7;
+//		CardTest c1 = new CardTest();
+//		c1.kind = "Heart";
+//		c1.number = 7;
+//		
+//		CardTest c2 = new CardTest();
+//		c2.kind =  "spade";
+//		c2.number = 4;
+//		
+//		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
+//		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
+//		
+//		c1.width = 30;
+//		c2.height = 40;
+//		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
+//		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
+//
+//	
 		
-		CardTest c2 = new CardTest();
-		c2.kind =  "spade";
-		c2.number = 4;
+		Data d = new Data();
+		d.x = 10;
+		System.out.println(d.x);
 		
-		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
-		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
-		
-		c1.width = 30;
-		c2.height = 40;
-		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
-		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
+		change(d);
+		System.out.println("after change: " + d.x);
 	}
+	
+	static void change(Data t) {
+		t.x = 1000;
+		System.out.println("change x:" + t.x);
+	}
+	
 }

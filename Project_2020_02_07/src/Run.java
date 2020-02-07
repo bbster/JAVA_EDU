3dfce458786dbe65e9801205bc8c244d63217422
bbster <bbster12@naver.com>
2020-02-07 16:37:01 +0900

    상속 exam

--- Project_2020_02_07/src/Run.java
+++ Project_2020_02_07/src/Run.java
@@ -2,16 +2,39 @@ public class Run {
 
 	public static void main(String[] args) {
 		
-		Tv tv;
-		tv = new Tv();
+//		 Tv tv = new Tv();
+//		 Tv tv1 = new Tv();
+//		
+//		System.out.println(tv.power);
+//		System.out.println(tv.channel);	
+//		tv.channel = 3;
+//		System.out.println(tv.channel);
+//	
+//		tv.channelup();
+//		System.out.println(tv.channel);
+//		System.out.printf("-----------%n");
+//		
+//		
+//		System.out.println(tv1.channel);
+//		tv1 = tv;
+//		System.out.println(tv1.channel);
 		
-		System.out.println(tv.power);
-		System.out.println(tv.channel);
+		//-----------------------------------------------------------------------------//
 		
-		tv.channel = 3;
-		System.out.println(tv.channel);
+		CardTest c1 = new CardTest();
+		c1.kind = "Heart";
+		c1.number = 7;
 		
-		tv.channelup();
-		System.out.println(tv.channel);
+		CardTest c2 = new CardTest();
+		c2.kind =  "spade";
+		c2.number = 4;
+		
+		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
+		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
+		
+		c1.width = 30;
+		c2.height = 40;
+		System.out.println("c1는 " + c1.kind +" " + c1.number + "이며 좌우폭은 " + c1.width + "이며, 높이는" + c1.height + "이다" );
+		System.out.println("c2는 " + c2.kind +" " + c2.number + "이며 좌우폭은 " + c2.width + "이며, 높이는" + c2.height + "이다" );
 	}
 }

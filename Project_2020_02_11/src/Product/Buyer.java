ec0854e69815f1b0fcf717a9c6c4181469eb613c
bbster <bbster12@naver.com>
2020-02-11 19:14:39 +0900

    복습하기 좋은 예제코드

--- Project_2020_02_11/src/Product/Buyer.java
+++ Project_2020_02_11/src/Product/Buyer.java
@@ -1,5 +1,7 @@
 package Product;
 
+import java.util.Vector;
+
 public class Buyer extends Product{
 
 	Buyer(int price) {
@@ -8,8 +10,9 @@ public class Buyer extends Product{
 
 	int money = 1000;
 	int bonusPoint = 0;
-	int i=0;
-	Product[] cart = new Product[10];
+	Vector cart = new Vector();
+//	int i=0;
+//	Product[] cart = new Product[10];
 	
 	
 	void buy(Product p) {
@@ -28,7 +31,9 @@ public class Buyer extends Product{
 			money -= p.price;
 			bonusPoint += p.bonusPoint;
 		}
-		cart[i++]=p;
+		
+//		cart[i++]=p;
+		cart.add(p);
 	}
 	
 	void showMoney() {
@@ -36,10 +41,45 @@ public class Buyer extends Product{
 	}
 	
 	void showCart() {
-		for(int i=0; i<cart.length; i++) {
-			Product pc = cart[i];
-			if(pc == null)break;
+		int sum = 0;
+		String cartList = "";
+		
+		if(cart.isEmpty()) {
+			System.out.println("구매한 내역이 없습니다.");
+			return;
+		}
+//		for(int i=0; i<cart.length; i++) {
+//			Product pc = cart[i];
+//			if(pc == null)break;
+//			System.out.println(pc.toString());
+		for(int i=0;i<cart.size();i++) {
+			Product pc = (Product)(cart.get(i));
+			sum += pc.price;
+			cartList += (i==0)?""+ pc.toString():", "+ pc.toString();
 			System.out.println(pc.toString());
 		}
+		
+		System.out.println(sum);
+		System.out.print("구입하신 물품은: "+cartList);
 	}
-}
+	
+	void returnProduct(Product p) {
+		
+		if(p instanceof Audio){
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+					
+		}
+		else if (p instanceof Computer) {
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+		}
+		else if (p instanceof Tv) {
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+		}
+		
+		cart.remove(p);
+		}
+	}
+

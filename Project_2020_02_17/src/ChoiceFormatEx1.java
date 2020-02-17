b52b89bccd530e790c57d71a399dee1440884ea5
bbster <bbster12@naver.com>
2020-02-17 17:06:22 +0900

    ChoiceFormat

---
+++ Project_2020_02_17/src/ChoiceFormatEx1.java
@@ -0,0 +1,17 @@
+import java.text.*;
+public class ChoiceFormatEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] limits = {60,70,80,90,};
+		String[] grades = {"D","C","B","A"};
+		
+		ChoiceFormat form = new ChoiceFormat(limits, grades);
+		
+		int[] scores = {100,95,88,70,52,60,70};
+		
+		for(int i=0; i<scores.length; i++){
+				System.out.println(scores[i] + " : " + form.format(scores[i]));
+		}
+	}
+}
